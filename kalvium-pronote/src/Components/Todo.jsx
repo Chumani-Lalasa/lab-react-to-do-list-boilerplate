ca10c48971b71f3d94ca8738698da3bb0750dfad
Chumani-Lalasa <chumanilalasa@gmail.com>
2024-01-23 11:09:50 +0530

    todo

---
+++ kalvium-pronote/src/Components/Todo.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+
+export class Todo extends Component {
+    constructor(){
+        super()
+        this.state = {
+            inputText : ' ',
+            todos : []
+        }
+    }
+    handleChange = (event) => {
+        let userInput = event.target.value;
+        this.setState({inputText:userInput});
+    }
+    handleClick = () => {
+        this.setState({
+            ...this.state,
+            todos : [...this.state.todos,this.state.inputText],
+            inputText: ' ' ,
+        });
+    }
+    handleUpdate = (index) => {
+        let updateInput = prompt("Update todo");
+        let filterTodos = this.state.todos.map((el,i) => {
+            if(i==index){
+                return updateInput;
+            }
+            return el;
+        });
+        this.setState({...this.state, todos: filterTodos});
+    }
+    handleDelete = (index) => {
+        let todos = this.state.todos
+        todos.splice(index,1)
+        this.setState({...this.setState, todos})
+
+    }
+  render() {
+    return (
+      <div>
+               <h1>Todo Application</h1>
+               <div className='main-container'>
+                    <input type="text" placeholder="Whats on your mind?"  onChange={this.handleChange} value={this.state.inputText}/>
+                    <button onClick={this.handleClick}>Add Todo</button>
+                    {
+                        this.state.todos.map((el,i) => {
+                            return(
+                                <div key={i}>
+                                    <p>{el}</p>
+                                    <button onClick={() => this.handleUpdate(i)}>Update</button>
+                                    <button onClick={() => this.handleDelete(i)}>Delete</button>
+                                </div>
+                            )
+                        })
+                    }
+               </div>
+      </div>
+    );
+  }
+}
+
+export default Todo;
